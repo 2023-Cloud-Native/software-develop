b6d299223eb52cbaeb5cd403e2400578fe86f1e7
dianel0922 <76809830+dianel0922@users.noreply.github.com>
2023-05-18 13:21:37 +0800

    push some resources

--- src/main.js
+++ src/main.js
@@ -4,6 +4,9 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap"; // 從nodeModule中載入Bootstrap
+import "bootstrap/dist/css/bootstrap.min.css"; // Import css file
+
 const app = createApp(App)
 
 app.use(router)
