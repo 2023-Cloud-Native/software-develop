04fc0c6de0c39d075c49adcf659e54a2fb27d053
dianel0922 <76809830+dianel0922@users.noreply.github.com>
2023-05-20 16:25:49 +0800

    overview frame 完成

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,10 +1,37 @@
-<script setup>
+<script>
+import { RouterLink, RouterView } from 'vue-router'
 
+export default{
+    data() {
+        return {
+            title: ["桃園", "竹科", "中科", "南科"]
+        }
+    }
+}
+    
 </script>
 
 <template>
     <div class="frame">
-        <div class="test"> this is the home view</div>
+        <div class="title">
+            <div class="content" v-for="name in title">{{ name }}</div>
+        </div>
+        <a class="water box" href="/water">
+            <div >
+                <div class="cl_name">水量(萬立方公尺)</div>
+            </div>
+        </a>
+        <a class="power box" href="/power">
+            <div >
+                <div class="cl_name">電力(萬瓩)</div>
+            </div>
+        </a>    
+        <a class="earthquake box" href="/earthquake">
+            <div >
+                <div class="cl_name">最近地震</div>
+            </div>
+        </a>
+        
     </div>
 </template>
 
@@ -12,12 +39,47 @@
 .frame{
     width: 100%;
     height: fit-content;
+    display: flex;
 }
 
-.test{
-    width: 1920px;
-    height: 1080px;
+.title{
+   width: 200px;
+   margin-top: 5%;
+   margin-right: 5%;
+   background-color: aqua;
+}
+
+.box{
+    display: block;
+    margin-right: 3%;
+}
+
+.power{
+    width: 25%;
+    background-color: pink;
+}
+
+.water{
+    width: 25%;
+    background-color: aquamarine;
+}
+
+.earthquake{
+    width: 25%;
+    background-color: bisque;
+}
+.content{
+    margin: auto;
+    padding: 25% 0%;
+}
+
+.cl_name{
+    text-decoration: none !important;
     text-align: center;
-    padding-top: 300px;
+    padding-top: 5%;
+    height: 5%;
+    color: black;
+    font-size: 25px;
+    font-weight: bold;
 }
 </style>
