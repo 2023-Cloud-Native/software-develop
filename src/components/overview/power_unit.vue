3ce2eedfa34877c0e82f16067efc56baae8009b9
dianel0922 <76809830+dianel0922@users.noreply.github.com>
2023-05-24 15:01:02 +0800

    overview frame complete

---
+++ src/components/overview/power_unit.vue
@@ -0,0 +1,135 @@
+<script>
+
+export default{
+    props: {
+        msg: {
+            type: Object, // 对象类型
+            default: () => ({ // 初始值为一个函数返回的对象
+            pow_gen:  1000.0,
+            pow_use: 500.0
+            })
+        }
+    },
+    data() {
+        return {
+           half_gen: 500.0,
+           pwesent_use: 0.0,
+           rot_deg: -90.0,
+           judge: "good" // good warning danger
+        }
+    },
+    methods: {
+        compute_data() {
+            this.half_gen = this.msg.pow_gen/2;
+            this.persent_use = this.msg.pow_use / this.msg.pow_gen;
+            var rotate = this.persent_use - 0.5;
+            if ( rotate  < 0) {
+                this.rot_deg =  -90.0;
+            }
+            else{
+                this.rot_deg = rotate * 2 * 180; // rotate * 2 * 100 * (180 / 100) 
+                this.rot_deg = this.rot_deg - 90;
+                // console.log(this.rot_deg, this.msg.pow_gen, this.msg.pow_use);
+            }
+            if(this.persent_use >= 0.75 && this.persent_use < 0.875) this.judge = "warning";
+            else if (this.persent_use >= 0.875) this.judge = "danger";
+            else this.judge = "good";
+            console.log(this.judge, this.persent_use);
+        },
+        format_out(val){
+            return Math.round(val*10) / 10;
+        }
+        
+    },
+    watch: {
+        msg() {
+        this.compute_data();
+        }
+    },
+    mounted() {
+        this.compute_data();
+    }
+    
+}
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="image_box">
+            <img class="bg_img" src="static/icon/power/pow.png">
+            <img class="ptr_img" :style="{ transform: 'translate(-50%, 50%) rotate(' + rot_deg + 'deg)' }" src="static/icon/power/pointer.png">
+            <div class="cur_use" :class="judge">{{ format_out(msg.pow_use) }} </div>
+            <div class="half_gen">{{ half_gen }}</div>
+            <div class="pow_gen" >{{ msg.pow_gen }}</div>
+        </div>
+
+        
+    </div>
+</template>
+
+
+<style scoped>
+.image_box{
+    position: relative;
+    width: 100%;
+    margin: 2%;
+    text-align: center;
+    display: block;
+}
+
+.bg_img{
+    position: absolute;
+    width: 75%;
+    height: 100%;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+    /* border: 1px solid red; */
+}
+
+.ptr_img{
+    position: absolute;
+    width: 20%;
+    height: 50%;
+    bottom: 50%;
+    left: 50%;
+    transform-origin: bottom center;
+    /* border: 1px solid blue; */
+}
+
+.cur_use{
+    position: absolute;
+    top: 10%;
+    left: 50%;
+    transform: translateX(-50%);
+    font-size: 20px;
+}
+
+.half_gen{
+    position: absolute;
+    top: 72%;
+    left: 31%;
+    transform: translate(-50% -50%);
+}
+.pow_gen{
+    position: absolute;
+    top: 72%;
+    left: 62%;
+    transform: translate(-50% -50%);
+}
+
+.good{
+    font-weight: 500;
+    color: black;
+}
+
+.warning{
+    font-weight: 1000;
+    color: goldenrod;
+}
+
+.danger{
+    font-weight: 1000;
+    color: red;
+}
+</style>
---
+++ src/components/overview/water_unit.vue
@@ -0,0 +1,132 @@
+<script>
+
+export default{
+    props: {
+        msg: {
+            type: Object, // 对象类型
+            default: () => ({ // 初始值为一个函数返回的对象
+            number: 1,
+            capacity: 1000.0,
+            storage_now: 1000.0,
+            storage_before: 1000.0
+            })
+        }
+    },
+    data() {
+        return {
+            img_route: "static/icon/water/yellow.png",
+            img_path: "static/icon/water/", //C:\Users\diane\cloud_frontend\src\assets\icon\water
+            img_name: [
+                "red.png",
+                "pink.png",
+                "yellow.png",
+                "green.png",
+                "blue.png"
+            ],
+            persent: 50.0,
+            change_class: 'same', // danger good same
+            change_persent: 0.0
+        }
+    },
+    methods: {
+        compute_data() {
+            // console.log("start")
+            var storage_now = this.msg.storage_now / this.msg.capacity;
+            var storage_before = this.msg.storage_before / this.msg.capacity;
+            this.persent = storage_now * 100.0;
+            var img_id = Math.floor(this.persent / 20);
+            if(img_id < 0) img_id = 0;
+            if(img_id > 4) img_id = 4;
+            this.img_route = this.img_path + this.img_name[img_id];
+            this.change_persent = (storage_now - storage_before) * 100;
+            if (this.change_persent > 0) {
+                this.change_class = 'good';
+            } else if (this.change_persent < 0) {
+                this.change_class = 'danger';
+            } else {
+                this.change_class = 'same';
+            }
+            this.change_persent = Math.abs(this.change_persent);
+        },
+        format_out(val){
+            return Math.round(val*10) / 10
+        }
+        
+    },
+    watch: {
+        msg() {
+        this.compute_data();
+        }
+    },
+    mounted() {
+        this.compute_data();
+    }
+    
+}
+</script>
+
+<template>
+    <div class="wrapper">
+        <div class="image_box">
+            <img class="image" :src="img_route">
+            <div class="water_amount" > {{ persent }} % </div>
+        </div>
+
+        <div class="box">
+            <div class="content" >水庫個數: &nbsp{{ msg.number }} </div>
+            <div class="content" >總水量: &nbsp{{ format_out(msg.capacity) }} </div>
+            <div class="content"  >現在水量: &nbsp{{ format_out(msg.storage_now)  }} </div>
+            <div class="content danger" v-if="change_class=='danger'" >相比昨日水量變化: <br>  下降{{ format_out(change_persent) }}% </div>
+            <div class="content good" v-else-if="change_class=='good'" >相比昨日水量變化: <br>  上升{{ format_out(change_persent)}}%</div>
+            <div class="content" v-else >相比昨日水量變化: <br>  上升0.0%</div>
+        </div>
+    </div>
+</template>
+
+
+<style scoped>
+.image_box{
+    position: relative;
+    width: 40%;
+    height: width;
+    margin: 2%;
+    text-align: center;
+    display: block;
+    /* border: 1px solid green; */
+}
+
+.image{
+    position: absolute;
+    top:50%;
+    left:80%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    height: 100%;
+}
+
+.box{
+    margin: auto;
+}
+
+.water_amount{
+    position: absolute;
+    top:50%;
+    left:80%;
+    transform: translate(-50%, -50%);
+    width: 100%;
+    font-size: 25px;
+    /* border: 1px solid sandybrown; */
+}
+
+.content{
+    color: black;
+    margin: auto;
+}
+.danger{
+    color: red;
+}
+
+.good{
+    color: blue;
+}
+</style>
