239d6a89feed2603c2e2aaa99e56096419ab5fcc
dianel0922 <76809830+dianel0922@users.noreply.github.com>
2023-06-06 23:05:26 +0800

    final v1

--- vite.config.js
+++ vite.config.js
@@ -26,5 +26,29 @@ export default defineConfig({
         rewrite: (path) => path.replace(/^\/api/, '')
       }
     }
+  },
+  build: {
+    port: 3000, // 指定客户端开发服务器的端口
+    proxy: {
+      '/api': {
+        target: 'http://220.135.239.117:28391/', // 代理服务器的地址 
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    // },
+    // rollupOptions: {
+    //   external: [
+    //     './static/icon/power/pow.png', 
+    //     './static/icon/power/pointer.png', 
+    //     './static/icon/water/red.png',
+    //     './static/icon/water/green.png',
+    //     './static/icon/water/pink.png',
+    //     './tatic/icon/water/blue.png',
+    //     './static/icon/water/yellow.png',
+    //     './static/icon/earthquake/green.png', 
+    //     './static/icon/earthquake/yellow.png',
+    //     './static/icon/earthquake/red.png'
+    //   ]
+    }
   }
 })
